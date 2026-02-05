157ab2c02495bbee2cb853144612ef6a5a5dcbfe
SoFaGucciMane <armuhametoarusan@gmail.com>
2026-02-05 22:40:35 +0500

    beta 0.1
    Version before adding a two-dimensional array

--- Scripts/BoardService.cs
+++ Scripts/BoardService.cs
@@ -11,20 +11,46 @@ public class BoardService : MonoBehaviour // В BoardService мы будет регистриров
     [SerializeField] RectTransform _boardRect;
     [SerializeField] Cell _cellPrefab;
     [SerializeField] private Sprite[] _cellSprites; // Массив спрайтов для ячеек
+
+    private MatchMachine _matchMachine; // Сылка
+
+    private void Awake()
+    {
+        _matchMachine = new MatchMachine(this);// Инициализируем из BoardServise 
+    }
     private void Start()
     {
+        VerifyBoardOnMatches();
+
         for (int x = 0; x < Config.BoardWith; x++)  // Заполнение самих ячеек.
         {
-            for (int y = 0; y < Config.BoardHeight ; y++)
+            for (int y = 0; y < Config.BoardHeight; y++)
             {
                 var cell = InstantiateCell();
                 var point = new Points(x, y);
                 cell.rect.anchoredPosition = GetBoardPositionsFromPoint(point);
                 var cellType = GetRandomCellType();
-                cell.Initialize(new CellData(cellType, point ), _cellSprites[(int)(cellType - 1)]);
+                cell.Initialize(new CellData(cellType, point), _cellSprites[(int)(cellType - 1)]);
             }
         }
     }
+
+    private void VerifyBoardOnMatches()
+    {
+        for (int x = 0; x < Config.BoardWith; x++)  // Заполнение самих ячеек.
+        {
+            for (int y = 0; y < Config.BoardHeight; y++)
+            {
+                //var point = new Points(x, y);
+                //var cellTypeAtPoint = GetCellTypeAtPoint(point);// Будет возврощать тип ячейки в заданной точке 
+            }
+        }
+    }
+
+    //private object GetCellTypeAtPoint(Points point) 
+    //{
+    //    _b
+    //}
 
     private CellData.CellType GetRandomCellType()
       =>(CellData.CellType)( Random.Range(0, _cellSprites.Length) + 1);
---
+++ Scripts/MathMachine.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+public class MatchMachine // Данная функция, будет проходить по всей доске и находить match готовые, чтобы предотвродить их совместое появлнеие заранее
+{
+    private readonly BoardService _boardService; // Сылка на скрипт, который отвечает за регенираюцию ячеек
+
+    public MatchMachine(BoardService boardService) // Создаем конструтор, чтобы матч машин могла получать данные о сетке и ячейках, координатах.
+    {
+        _boardService = boardService;
+    }
+
+    public List<Points> GetMathedPoins(Points point, bool main) // Будет проверять схему с начальной точки и выписвать уже где есть матчи. 
+    {
+        return null;
+    }
+}
+
