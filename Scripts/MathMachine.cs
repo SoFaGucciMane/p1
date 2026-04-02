fdc19d1da3a67ede9633c86222ee6ca4aed430c6
SoFaGucciMane <armuhametoarusan@gmail.com>
2026-04-02 10:36:41 +0500

    Update MathMachine.cs

--- Scripts/MathMachine.cs
+++ Scripts/MathMachine.cs
@@ -1,15 +1,15 @@
 ﻿using System.Collections.Generic;
 
-public class MatchMachine // Данная функция, будет проходить по всей доске и находить match готовые, чтобы предотвродить их совместое появлнеие заранее
+public class MatchMachine 
 {
-    private readonly BoardService _boardService; // Сылка на скрипт, который отвечает за регенираюцию ячеек
+    private readonly BoardService _boardService; 
 
-    public MatchMachine(BoardService boardService) // Создаем конструтор, чтобы матч машин могла получать данные о сетке и ячейках, координатах.
+    public MatchMachine(BoardService boardService) 
     {
         _boardService = boardService;
     }
 
-    public List<Points> GetMathedPoins(Points point, bool main) // Будет проверять схему с начальной точки и выписвать уже где есть матчи. 
+    public List<Points> GetMathedPoins(Points point, bool main) 
     {
         return null;
     }
