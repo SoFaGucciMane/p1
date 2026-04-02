c0b4fa9343e0ef2ff57e83c7b520390ffed34496
SoFaGucciMane <armuhametoarusan@gmail.com>
2026-04-02 10:36:15 +0500

    Refactor Cell class properties and methods

--- Scripts/Cell.cs
+++ Scripts/Cell.cs
@@ -8,15 +8,15 @@ public class Cell : MonoBehaviour
 {
 
 
-    public RectTransform rect; // »нформци€ о каждой €чейки хранитьс€ тут.
+    public RectTransform rect; 
 
     [SerializeField] private UnityEngine.UI.Image _image;
 
     private CellData _cellData;
 
-    public Points Points => _cellData.point; // ƒл€ почени€ данных координат €чеек с CellData
-    public CellData.CellType CellType => _cellData.cellType; // ƒл€ почени€ данных €чеек с CellData
-    public void Initialize(CellData cellData,  Sprite sprite) // »нициализуем и передаем данные о каждой €чейки
+    public Points Points => _cellData.point; 
+    public CellData.CellType CellType => _cellData.cellType; 
+    public void Initialize(CellData cellData,  Sprite sprite) 
     {
         _cellData = cellData;
 
@@ -24,7 +24,7 @@ public class Cell : MonoBehaviour
         UpdateName();
     }
 
-    private void UpdateName() // —оздаем функцию, дл€ иницилизации в какой €чейке наход€тьс€ спрайты
+    private void UpdateName() 
     {
         transform.name = $"Cell[{Points.x}, {Points.y}]";
     }
